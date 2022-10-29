07a00c8e16580221da6851ea6c8bddaa2f4b8d85
bete7512 <betekbebe@gmail.com>
2022-10-29 12:52:09 +0300

    message for task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,13 +1,15 @@
 $(document).ready(function (){
-
     let amenities = []
+    let amenities_id = []
     let count = 0;
    let elements =  $(".special")
    for (elem of elements) {
     let name = $(elem).attr("data-name");
+    let id = $(elem).attr("data-id");
     $(elem).change(function(){
         if (this.checked)
         {
+            amenities_id.push(id)
             amenities.push(name);
             $(".amenities h4").text(amenities.join())
         }
@@ -17,26 +19,6 @@ $(document).ready(function (){
             amenities.splice(index, 1);
             $(".amenities h4").text(amenities.join())
         }
-      
-        
-        // if (this.checked){
-            
-        //     let name = String($(this).attr("data-name"));
-        //     alert("name is", name);
-        //     amenities.push($(".amenities h4").append($(this).attr("data-name")))
-        //    // $(".amenities h4").text(amenities.join(", "))
-        //    alert(amenities.join())
-
-        // }
-        // else
-        // {
-            
-        //     let index = amenities.indexOf($(this.attr("data-name")))
-        //     alert(index);
-        //     amenities.splice(index, 1)
-        //     //$(".amenities h4").text(amenities.join(", "))
-        //     alert(amenities.join())
-        // }
     })
    }
    $.ajax({
@@ -59,27 +41,54 @@ $(document).ready(function (){
     }
 });
 
-$.ajax({
-    type: "post",
-    url: "http://0.0.0.0:5001/api/v1/places_search/",
-    data: JSON.stringify({}),
-    contentType: "application/json",
-    success: function(data) {
+// $.ajax({
+//     type: "post",
+//     url: "http://0.0.0.0:5001/api/v1/places_search/",
+//     data: JSON.stringify({}),
+//     contentType: "application/json",
+//     success: function(data) {
       
-        for (let d of data){
-            console.log(d)
-            $(".places").append("<article><div class='title_box'><h2>"+ 
-            d.name+"</h2><div class='price_by_night'>"+ 
-            d.price_by_night +"</div></div><div class='information'><div class='max_guest'>" + d.max_guest + "Guest" + (d.max_guest > 1 ? "s": "") +
-            "</div><div class='number_rooms'>" + d.number_rooms + "Bedroom" + (d.number_rooms != 1 ? "s": "") + 
-            "</div><div class='number_bathrooms'>" + d.number_bathrooms + "Bathroom" + 
-            (d.number_bathrooms != 1 ?"s": "") + "</div></div></article>")
-        }
-    },
-    error: function(err) {
-        console.log(err)
-    }
+//         for (let d of data){
+//             console.log(d)
+//             $(".places").append("<article><div class='title_box'><h2>"+ 
+//             d.name+"</h2><div class='price_by_night'>"+ 
+//             d.price_by_night +"</div></div><div class='information'><div class='max_guest'>" + d.max_guest + "Guest" + (d.max_guest > 1 ? "s": "") +
+//             "</div><div class='number_rooms'>" + d.number_rooms + "Bedroom" + (d.number_rooms != 1 ? "s": "") + 
+//             "</div><div class='number_bathrooms'>" + d.number_bathrooms + "Bathroom" + 
+//             (d.number_bathrooms != 1 ?"s": "") + "</div></div></article>")
+//         }
+//     },
+//     error: function(err) {
+//         console.log(err)
+//     }
 
-})
+// })
+
+
+// function get_search(){
+    $('button').click(()=>{
+        console.log(amenities_id);
+        $.ajax({
+            type:"POST",
+            url:"http://0.0.0.0:5001/api/v1/places_search/",
+            data:JSON.stringify({"amenitiess":amenities_id}),
+            contentType: "application/json",
+            success: (data)=>{
+                console.log(data);
+                for (let d of data){
+                    // console.log(d)
+                    $(".places").append("<article><div class='title_box'><h2>"+ 
+                    d.name+"</h2><div class='price_by_night'>"+ 
+                    d.price_by_night +"</div></div><div class='information'><div class='max_guest'>" + d.max_guest + "Guest" + (d.max_guest > 1 ? "s": "") +
+                    "</div><div class='number_rooms'>" + d.number_rooms + "Bedroom" + (d.number_rooms != 1 ? "s": "") + 
+                    "</div><div class='number_bathrooms'>" + d.number_bathrooms + "Bathroom" + 
+                    (d.number_bathrooms != 1 ?"s": "") + "</div></div></article>")
+                }
+            },
+            error:(err)=>{console.log(err);
+            }
+        })
+    })
+// }
 
 })
